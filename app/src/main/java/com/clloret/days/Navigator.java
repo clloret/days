22005d2f4f19423e064361fce07d62d306d5546f
Carlos Lloret <clloretplayan@gmail.com>
2018-11-11 23:45:06 +0100

    Fix SettingsActivity package

--- app/src/main/java/com/clloret/days/Navigator.java
+++ app/src/main/java/com/clloret/days/Navigator.java
@@ -2,11 +2,11 @@ package com.clloret.days;
 
 import android.content.Context;
 import android.content.Intent;
-import com.clloret.days.activities.SettingsActivity;
 import com.clloret.days.events.create.EventCreateActivity;
 import com.clloret.days.events.edit.EventEditActivity;
 import com.clloret.days.model.entities.EventViewModel;
 import com.clloret.days.model.entities.TagViewModel;
+import com.clloret.days.settings.SettingsActivity;
 import com.clloret.days.tags.edit.TagEditActivity;
 
 public class Navigator {
