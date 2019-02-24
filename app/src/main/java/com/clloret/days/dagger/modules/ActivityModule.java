18ef68262d2b7d6b3a8fc327d68c95a8865af266
Carlos Lloret <clloretplayan@gmail.com>
2019-02-24 19:27:19 +0100

    Add ActivityScope to ActivityModule

--- app/src/main/java/com/clloret/days/dagger/modules/ActivityModule.java
+++ app/src/main/java/com/clloret/days/dagger/modules/ActivityModule.java
@@ -1,6 +1,7 @@
 package com.clloret.days.dagger.modules;
 
 import com.clloret.days.activities.MainActivity;
+import com.clloret.days.dagger.scopes.ActivityScope;
 import com.clloret.days.events.create.EventCreateActivity;
 import com.clloret.days.events.edit.EventEditActivity;
 import com.clloret.days.events.list.EventListFragment;
@@ -14,23 +15,30 @@ import dagger.android.ContributesAndroidInjector;
 public abstract class ActivityModule {
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract MainActivity bindMainActivity();
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract EventCreateActivity bindEventCreateActivity();
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract EventEditActivity bindEventEditActivity();
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract EventListFragment bindEventListFragment();
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract MenuFragment bindMenuFragment();
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract TagCreateActivity bindTagCreateActivity();
 
   @ContributesAndroidInjector()
+  @ActivityScope
   abstract TagEditActivity bindTagEditActivity();
 }
