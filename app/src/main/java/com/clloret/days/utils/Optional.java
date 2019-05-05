18e24c52c4c848f08315849320ea76f44af6b07b
Carlos Lloret <clloretplayan@gmail.com>
2019-05-05 21:06:52 +0200

    Add Optional class to avoid the use of null values

---
+++ app/src/main/java/com/clloret/days/utils/Optional.java
@@ -0,0 +1,55 @@
+package com.clloret.days.utils;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+@SuppressWarnings({"PMD.NullAssignment", "PMD.ShortMethodName"})
+public class Optional<T> {
+
+  private T value;
+
+  private Optional() {
+
+    this.value = null;
+  }
+
+  private Optional(T value) {
+
+    this.value = Objects.requireNonNull(value);
+  }
+
+  public static <T> Optional<T> empty() {
+
+    return new Optional<>();
+  }
+
+  public static <T> Optional<T> of(T value) {
+
+    return new Optional<>(value);
+  }
+
+  public void ifPresent(Action<T> action) {
+
+    if (value != null) {
+      action.apply(value);
+    }
+  }
+
+  public T get() {
+
+    if (value == null) {
+      throw new NoSuchElementException("No value present");
+    }
+    return value;
+  }
+
+  public boolean isPresent() {
+
+    return value != null;
+  }
+
+  public interface Action<T> {
+
+    void apply(T value);
+  }
+}
