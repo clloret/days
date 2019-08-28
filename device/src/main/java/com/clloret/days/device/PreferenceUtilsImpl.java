07a9e612b65744b7be36b1b1e3125693dc38683a
Carlos Lloret <clloretplayan@gmail.com>
2019-08-28 18:17:51 +0200

    Add preferences to PreferenceUtils

--- device/src/main/java/com/clloret/days/device/PreferenceUtilsImpl.java
+++ device/src/main/java/com/clloret/days/device/PreferenceUtilsImpl.java
@@ -3,6 +3,7 @@ package com.clloret.days.device;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import com.clloret.days.domain.events.order.EventSortFactory.SortType;
 import com.clloret.days.domain.utils.PreferenceUtils;
 import com.clloret.days.domain.utils.StringResourceProvider;
 import javax.inject.Inject;
@@ -12,6 +13,11 @@ import javax.inject.Singleton;
 public class PreferenceUtilsImpl implements PreferenceUtils {
 
   private static final int DEFAULT_REMINDER_TIME = 0;
+  private static final int DEFAULT_SORT_MODE = SortType.NAME.getValue();
+  private static final boolean DEFAULT_USE_REMOTE_DATASTORE = false;
+  private static final String DEFAULT_LIST = "0";
+  private static final String DEFAULT_AIRTABLE_API_KEY = "";
+  private static final String DEFAULT_AIRTABLE_BASE_IDE = "";
 
   private final StringResourceProvider stringResourceProvider;
   private final SharedPreferences preferences;
@@ -25,10 +31,51 @@ public class PreferenceUtilsImpl implements PreferenceUtils {
   }
 
   @Override
+  public boolean getUseRemoteDataStore() {
+
+    return preferences
+        .getBoolean(stringResourceProvider.getPrefRemoteDatastore(), DEFAULT_USE_REMOTE_DATASTORE);
+  }
+
+  @Override
   public int getReminderTime() {
 
     return preferences
         .getInt(stringResourceProvider.getPrefReminderTimeKey(), DEFAULT_REMINDER_TIME);
+  }
 
+  @Override
+  public int getSortMode() {
+
+    return preferences
+        .getInt(stringResourceProvider.getPrefSortMode(), DEFAULT_SORT_MODE);
+  }
+
+  @Override
+  public void setSortMode(SortType sortType) {
+
+    preferences.edit().putInt(stringResourceProvider.getPrefSortMode(), sortType.getValue())
+        .apply();
+  }
+
+  @Override
+  public String getAirtableApiKey() {
+
+    return preferences
+        .getString(stringResourceProvider.getPrefAirtableApiKey(), DEFAULT_AIRTABLE_API_KEY);
+  }
+
+  @Override
+  public String getAirtableBaseId() {
+
+    return preferences
+        .getString(stringResourceProvider.getPrefAirtableBaseId(), DEFAULT_AIRTABLE_BASE_IDE);
+  }
+
+  @Override
+  public String getDefaultList() {
+
+    return preferences
+        .getString(stringResourceProvider.getPrefDefaultList(), DEFAULT_LIST);
   }
 }
--- domain/src/main/java/com/clloret/days/domain/utils/PreferenceUtils.java
+++ domain/src/main/java/com/clloret/days/domain/utils/PreferenceUtils.java
@@ -1,6 +1,20 @@
 package com.clloret.days.domain.utils;
 
+import com.clloret.days.domain.events.order.EventSortFactory.SortType;
+
 public interface PreferenceUtils {
 
+  boolean getUseRemoteDataStore();
+
   int getReminderTime();
+
+  int getSortMode();
+
+  void setSortMode(SortType sortType);
+
+  String getAirtableApiKey();
+
+  String getAirtableBaseId();
+
+  String getDefaultList();
 }
