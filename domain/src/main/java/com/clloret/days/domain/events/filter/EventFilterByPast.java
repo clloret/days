52a7f026b6f618599542aefff470d9678d8b9db1
Carlos Lloret <clloretplayan@gmail.com>
2019-12-21 19:31:05 +0100

    Add EventFilterByPast and test

--- domain/src/main/java/com/clloret/days/domain/events/filter/EventFilterByPast.java
+++ domain/src/main/java/com/clloret/days/domain/events/filter/EventFilterByPast.java
@@ -3,14 +3,37 @@ package com.clloret.days.domain.events.filter;
 import com.clloret.days.domain.entities.Event;
 import com.clloret.days.domain.repository.EventRepository;
 import io.reactivex.Single;
+import java.util.Date;
 import java.util.List;
+import org.joda.time.LocalDate;
 
 public class EventFilterByPast extends EventFilterStrategy {
+
+  private final LocalDate date;
+
+  public EventFilterByPast(LocalDate date) {
+
+    this.date = date;
+  }
 
   @Override
   public Single<List<Event>> getEvents(EventRepository appDataStore) {
 
-    // TODO: 26/02/2018 Implement
-    return null;
+    return appDataStore.getBeforeDate(date);
   }
+
+  @Override
+  public boolean eventMatchFilter(Event event) {
+
+    Date eventDate = event.getDate();
+
+    if (eventDate == null) {
+      return false;
+    }
+
+    LocalDate localDate = new LocalDate(event.getDate());
+
+    return localDate.isBefore(date);
+  }
+
 }
