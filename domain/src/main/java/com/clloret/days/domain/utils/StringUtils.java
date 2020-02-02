02d5d7edda27d2559f2878c51405371d4e04cba8
Carlos Lloret <clloretplayan@gmail.com>
2020-02-02 20:45:36 +0100

    Add capitalizeFirstLetter to StringUtils

--- domain/src/main/java/com/clloret/days/domain/utils/StringUtils.java
+++ domain/src/main/java/com/clloret/days/domain/utils/StringUtils.java
@@ -25,4 +25,9 @@ public class StringUtils {
         .replaceAll("[^\\p{ASCII}]", "");
   }
 
+  public static String capitalizeFirstLetter(String text) {
+
+    return text.substring(0, 1).toUpperCase() + text.substring(1);
+  }
+
 }
