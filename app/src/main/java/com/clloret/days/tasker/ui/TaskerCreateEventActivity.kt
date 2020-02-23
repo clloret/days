002b5c07a0b47bbf5921f914541bc767286bc6ed
Carlos Lloret <clloretplayan@gmail.com>
2020-02-23 13:35:20 +0100

    Add TaskerCreateEventActivity

---
+++ app/src/main/java/com/clloret/days/tasker/ui/TaskerCreateEventActivity.kt
@@ -0,0 +1,13 @@
+package com.clloret.days.tasker.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.clloret.days.R
+
+class TaskerCreateEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasker_create_event)
+    }
+}
