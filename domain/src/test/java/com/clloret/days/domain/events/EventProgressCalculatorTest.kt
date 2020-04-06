946b3db04ee02e0424bfd3f097b6177c84452ab2
Carlos Lloret <clloretplayan@gmail.com>
2020-04-06 22:20:43 +0200

    Add EventProgressCalculator tests

---
+++ domain/src/test/java/com/clloret/days/domain/events/EventProgressCalculatorTest.kt
@@ -0,0 +1,86 @@
+package com.clloret.days.domain.events
+
+import com.clloret.days.domain.entities.EventBuilder
+import com.clloret.days.domain.utils.TimeProvider
+import com.google.common.truth.Truth.assertThat
+import org.joda.time.LocalDate
+import org.junit.Before
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class EventProgressCalculatorTest {
+
+  private val today = LocalDate()
+          .withYear(2020)
+          .withMonthOfYear(4)
+          .withDayOfMonth(6)
+
+  private val progressDate1 = LocalDate()
+          .withDayOfMonth(1)
+          .withMonthOfYear(4)
+          .withYear(2020)
+          .toDate()
+
+  private val progressDate2 = LocalDate()
+          .withDayOfMonth(16)
+          .withMonthOfYear(4)
+          .withYear(2020)
+          .toDate()
+
+  private val latestDate = LocalDate()
+          .withDayOfMonth(30)
+          .withMonthOfYear(4)
+          .withYear(2020)
+          .toDate()
+
+  private val oldestDate = LocalDate()
+          .withDayOfMonth(1)
+          .withMonthOfYear(4)
+          .withYear(2020)
+          .toDate()
+
+  private var futureEvent = EventBuilder()
+          .setDate(latestDate)
+          .build()
+
+  private var pastEvent = EventBuilder()
+          .setDate(oldestDate)
+          .build()
+
+  @Mock
+  lateinit var timeProvider: TimeProvider
+
+  @InjectMocks
+  lateinit var sut: EventProgressCalculator
+
+  @Before
+  fun init() {
+    MockitoAnnotations.initMocks(this)
+
+    Mockito.`when`(timeProvider.currentDate).thenReturn(today)
+  }
+
+  @Test
+  fun calculateEventProgress_WhenDateIsAfter_ReturnCorrectValues() {
+    futureEvent.progressDate = progressDate1
+
+    val progress = sut.calculateEventProgress(futureEvent)
+
+    assertThat(progress.progress).isEqualTo(5)
+    assertThat(progress.max).isEqualTo(29)
+  }
+
+  @Test
+  fun calculateEventProgress_WhenDateIsBefore_ReturnCorrectValues() {
+    pastEvent.progressDate = progressDate2
+
+    val progress = sut.calculateEventProgress(pastEvent)
+
+    assertThat(progress.progress).isEqualTo(5)
+    assertThat(progress.max).isEqualTo(15)
+  }
+
+}
