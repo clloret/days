94c4cfea90f747202b50e01c0073816049b1bb54
Carlos Lloret <clloretplayan@gmail.com>
2020-04-14 20:45:48 +0200

    Mock EventProgressCalculator in the event interactors tests

--- domain/src/test/java/com/clloret/days/domain/interactors/events/EditEventUseCaseTest.java
+++ domain/src/test/java/com/clloret/days/domain/interactors/events/EditEventUseCaseTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import com.clloret.days.domain.entities.Event;
 import com.clloret.days.domain.entities.Event.TimeUnit;
 import com.clloret.days.domain.entities.EventBuilder;
+import com.clloret.days.domain.events.EventProgressCalculator;
 import com.clloret.days.domain.interactors.events.EditEventUseCase.RequestValues;
 import com.clloret.days.domain.reminders.EventReminderManager;
 import com.clloret.days.domain.repository.EventRepository;
@@ -27,6 +28,9 @@ public class EditEventUseCaseTest {
 
   @Mock
   private EventReminderManager eventReminderManager;
+
+  @Mock
+  private EventProgressCalculator eventProgressCalculator;
 
   @Spy
   private ThreadSchedulers threadSchedulers = new RxImmediateThreadingSchedulers();
