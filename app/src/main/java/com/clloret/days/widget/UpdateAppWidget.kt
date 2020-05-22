322aa08974985f2a875cf5da644558150aec0790
Carlos Lloret <clloretplayan@gmail.com>
2020-05-22 18:18:27 +0200

    Show the event view when click in the widget

--- app/src/main/java/com/clloret/days/widget/UpdateAppWidget.kt
+++ app/src/main/java/com/clloret/days/widget/UpdateAppWidget.kt
@@ -1,10 +1,14 @@
 package com.clloret.days.widget
 
 import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.clloret.days.R
+import com.clloret.days.activities.MainActivity
+import com.clloret.days.device.notifications.NotificationsIntents
 import com.clloret.days.domain.entities.Event
 import com.clloret.days.domain.events.EventPeriodFormat
 import com.clloret.days.domain.interactors.events.GetEventUseCase
@@ -14,7 +18,9 @@ import javax.inject.Inject
 
 class UpdateAppWidget @Inject constructor(
         private val getEventUseCase: GetEventUseCase,
-        private val eventPeriodFormat: EventPeriodFormat) {
+        private val eventPeriodFormat: EventPeriodFormat,
+        private val notificationsIntents: NotificationsIntents
+) {
 
   @SuppressLint("CheckResult")
   fun update(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
@@ -26,22 +32,30 @@ class UpdateAppWidget @Inject constructor(
               .subscribe({ event: Event ->
 
                 val daysSinceFormatted = eventPeriodFormat.getDaysSinceFormatted(event.date)
+                val pendingIntent = notificationsIntents.getViewEventIntent(event)
 
-                showWidgetValues(daysSinceFormatted, event.name, context, appWidgetManager, appWidgetId)
+                showWidgetValues(daysSinceFormatted, event.name, pendingIntent, context,
+                        appWidgetManager, appWidgetId)
               })
               { Timber.e(it) }
     } ?: run {
-      showWidgetValues(UNKNOWN_EVENT_DAYS, UNKNOWN_EVENT_NAME, context, appWidgetManager, appWidgetId)
-    }
+      val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+              .let { intent ->
+                PendingIntent.getActivity(context, 0, intent, 0)
+              }
 
+      showWidgetValues(UNKNOWN_EVENT_DAYS, UNKNOWN_EVENT_NAME, pendingIntent, context,
+              appWidgetManager, appWidgetId)
+    }
   }
 
-  private fun showWidgetValues(days: String?, eventName: String, context: Context,
-                               appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+  private fun showWidgetValues(days: String, eventName: String, pendingIntent: PendingIntent,
+                               context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
 
     RemoteViews(context.packageName, R.layout.days_widget).apply {
       setTextViewText(R.id.textViewEventDays, days)
       setTextViewText(R.id.textViewEventName, eventName)
+      setOnClickPendingIntent(R.id.layoutWidget, pendingIntent)
       appWidgetManager.updateAppWidget(appWidgetId, this)
     }
   }
